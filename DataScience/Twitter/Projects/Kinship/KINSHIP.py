76207aa1bf8196ab5015a0323dfaa200ca539ce3
likarajo <likarajo@gmail.com>
2017-07-12 22:20:09 +0530

    Update KINSHIP.py

--- DataScience/Twitter/Projects/Kinship/KINSHIP.py
+++ DataScience/Twitter/Projects/Kinship/KINSHIP.py
@@ -3,11 +3,11 @@ sys.path.insert(0, './lib/')
 from operations import kinships
 
 kinships(
-        ACCESS_TOKEN = "2390269165-bTMxMFUrJma8etDMHoB6uTOl8WElxLMPp3WDRFE",
-        ACCESS_SECRET = "yEhAL19G3sgOAtzioBYyV1CLrx93GtZYhowWsLJARZwmA",
-        CONSUMER_KEY = "jz0ikcqx7F4VH7haLxn16rOH4",
-        CONSUMER_SECRET = "Y5jHZdHguM3YtDcH2RED1KZcepjkenNMOtGZF1XMZ9RG9oddUc",
+        ACCESS_TOKEN = "xxxxxxxx",
+        ACCESS_SECRET = "xxxxxxxx",
+        CONSUMER_KEY = "xxxxxxxx",
+        CONSUMER_SECRET = "xxxxxxxx",
         word = 'Wimbledon',
-        kins = 5,
-        tweet_count = 20
+        kins = 20,
+        tweet_count = 1000
 )
