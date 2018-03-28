1aaf6c9c271452c24795c1a1057d6ee2087032e7
Rajarshi Chattopadhyay <likarajo@gmail.com>
2018-03-28 22:51:30 +0530

    update google analytics script

--- assets/scripts/acclaim.js
+++
@@ -1,8 +0,0 @@
-(function() {
-	var s = document.createElement('script');
-	s.type = 'text/javascript';
-	s.async = true;
-	s.src = '//cdn.youracclaim.com/assets/utilities/embed.js';
-	var o = document.getElementsByTagName('script')[0];
-	o.parentNode.insertBefore(s, o);
-})();
