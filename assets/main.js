1fdc92342778b2768100f1136e15587f8ed871fd
Rajarshi Chattopadhyay <likarajo@gmail.com>
2018-04-23 18:40:30 +0530

    modify flex content

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+function showMore() {
+  document.getElementById("more").style.display="block";
+  document.getElementById("showMoreLink").style.display="none";
+}
+
+function hideMore() {
+  document.getElementById("more").style.display="none";
+  document.getElementById("showMoreLink").style.display="block";
+}
