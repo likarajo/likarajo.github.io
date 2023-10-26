66b4493da9488c6182f84f143860b975592e24bd
Rajarshi C <rchattopadhyay.teslamotors.com>
2023-10-26 21:37:02 +0530

    contact

---
+++ src/components/contact/FormValidation.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+
+const initialFormValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    message:"",
+    formSubmitted: false,
+    success: false
+}
+  
+export const useFormControls = () => {
+    
+    const [values, setValues] = useState(initialFormValues);
+    const [errors, setErrors] = useState({});
+    
+    const handleInputValue = (e) => {
+        const { name, value } = e.target;
+        setValues({
+            ...values,
+            [name]: value
+        });
+        validate({ [name]: value })
+    }
+
+    const validate = (fieldValues) => {
+        let temp = { ...errors }
+        if ("firstName" in fieldValues)
+            temp.firstName = fieldValues.firstName ? "" : "This field is required."
+        if ("email" in fieldValues) {
+            temp.email = fieldValues.email ? "" : "This field is required."
+            if (fieldValues.email)
+                temp.email = /^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(fieldValues.email) ? "" : "Email is not valid."
+        }
+        if ("message" in fieldValues)
+        temp.message =
+            fieldValues.message ? "" : "This field is required."
+        setErrors({
+            ...temp
+        });
+    }
+    
+    const formIsValid = () => {
+        const isValid =
+            values.firstName &&
+            values.email &&
+            values.message &&
+            Object.values(errors).every((x) => x === "");
+        return isValid;
+    };
+
+    const handleFormSubmit = async (e) => {
+        e.preventDefault();
+        if (formIsValid()) {
+            //await postContactForm(values);
+            alert("You've sent the message!")
+        }
+    };
+   
+    return {
+        handleInputValue,
+        handleFormSubmit,
+        formIsValid,
+        errors
+    };
+
+}
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,18 @@
-function Contact() {
+import { NavigateNext } from "@mui/icons-material"
+import { Breadcrumbs, Link, Typography } from "@mui/material"
+import Form from "../components/contact/Form"
 
+function Contact() {
+    const breadcrumbs = [
+        <Link key="1" color="inherit" style={{textDecoration: "none"}} href="/"><b>Home</b></Link>,
+        <Typography key="2">Contact</Typography>
+    ]
     return (
         <div id="contact">
-            Contact
+            <Breadcrumbs separator={<NavigateNext fontSize="small"/>} style={{maxWidth: "280px", padding: "10px"}}>
+                {breadcrumbs}
+            </Breadcrumbs>
+            <Form />
         </div>
     )
 }
