d1c62790210fcc0b4d8abddfb53dafb9dd4bef03
Rajarshi C <rchattopadhyay.teslamotors.com>
2023-10-29 09:47:48 +0530

    update about

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -5,7 +5,7 @@ function About() {
         <Card variant="outlined" style={{ padding: '30px 20px', margin: '0 auto', width:'40%', minWidth:'290px', maxWidth:'700px'}}>
             <Avatar src="images/site/profile.jpg" alt="likarajo" style={{ width: "60px", height: "60px", margin: "0 auto", marginTop: -20, marginBottom: 5}}/>
             <Typography style={{display: 'block'}}>
-                I'm a software developer by profession, and I love traveling whenever I can.
+                Being a software developer by profession, I love traveling and indulging in culinary activities whenever I can. 
                 Whether it's learning about technology, exploring cities, hiking through beautiful landscapes, trying various cuisines, 
                 or immersing myself in different cultures, I like documenting my experiences to share my inspiration and journey. 
                 Through that, I want to help others find value in their own journeys.
--- src/components/profession/Cards.jsx
+++ src/components/profession/Cards.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { useMediaQuery } from "react-responsive";
-import { Card, CardMedia, Stack, Typography } from "@mui/material";
+import { Card, CardMedia, Dialog, DialogContent, DialogTitle, IconButton, Stack, Typography } from "@mui/material";
 import AppButton from "../common/AppButton";
-import { Description, LinkedIn, Star } from "@mui/icons-material";
+import { Description, InfoRounded, LinkedIn, Star } from "@mui/icons-material";
 
 function Cards() {
 
@@ -12,13 +12,16 @@ function Cards() {
         setIsDesktop(desktopSize);
     });
 
+    const [openInfo, setOpenInfo] = useState(false);
+
     return (
         <div style={{paddingTop: isDesktop?'50px':'20px'}}>
         {isDesktop ?
             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: 'auto', width:'60%', gap: '10%', minWidth:'600px'}}>
                 <div style={{width: '45%'}}>
-                    <Card style={{width: '100%', aspectRatio: '4/3', borderRadius: 10}}>
-                        <CardMedia component="img" image="images/profession/linkedin.jpg" alt="linkedin" /> 
+                    <Card style={{width: '100%', aspectRatio: '4/3', borderRadius: 10, position: 'relative'}}>
+                        <CardMedia component="img" image="images/profession/linkedin.jpg" alt="linkedin" />
+                        <IconButton size="large" style={{position: 'absolute', right: '2px', bottom: '3%'}} onClick={() => setOpenInfo(true)}><InfoRounded/></IconButton>
                     </Card>
                     <Stack direction="row" spacing={1} style={{marginTop: "10px"}}>
                         <AppButton variant="contained" href="https://www.linkedin.com/in/likarajo/" target="_blank"><LinkedIn/>LinkedIn</AppButton>
@@ -30,8 +33,9 @@ function Cards() {
         :
             <div style={{ justifyContent: 'center', margin: '20px auto', width:'45%', minWidth:'280px'}}>
                 <div style={{width: '100%', margin: '20px 0'}}>
-                    <Card style={{width: '100%', aspectRatio: '4/3', borderRadius: 10}}>
+                    <Card style={{width: '100%', aspectRatio: '4/3', borderRadius: 10, position: 'relative'}}>
                         <CardMedia component="img" image="images/profession/linkedin.jpg" alt="linkedin" />
+                        <IconButton size="large" style={{position: 'absolute', right: '2px', bottom: '3%'}} onClick={() => setOpenInfo(true)}><InfoRounded/></IconButton>
                     </Card>
                     <Stack direction="row" spacing={1} style={{marginTop: "10px"}}>
                         <AppButton variant="contained" href="https://www.linkedin.com/in/likarajo/" target="_blank"><LinkedIn/>LinkedIn</AppButton>
@@ -41,6 +45,18 @@ function Cards() {
                 </div>
             </div>
         }
+        <Dialog open={openInfo} onClose={() => setOpenInfo(false)}>
+            <DialogTitle><Typography variant="h6">About</Typography></DialogTitle>
+            <DialogContent>
+                <Typography>
+                    I am passionate about giving back to the community through the design and development of Intelligent Systems 
+                    that can solve real-life problems. With expertise in full-stack application development, 
+                    I am currently working with multiple business units within the organization on the design and architecture of software systems and data pipelines.
+                    Being a self-motivated team player, with leadership, organizational, communication, and presentation skills, 
+                    I keep myself relevant by augmenting my skills with the latest technologies.
+                </Typography>
+            </DialogContent>
+        </Dialog>
         </div>    
     )
 }
