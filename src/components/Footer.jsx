f695b31524be1b62e27bee83f227dae9d95a69e1
Rajarshi C <rchattopadhyay@tesla.com>
2025-04-05 19:11:02 -0500

    footer year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,13 @@ import Links from '../components/footer/Links'
 
 function Footer() {
 
+  const year = new Date().getFullYear();
+
   return (
     <div style={{justifyContent:'center', height:'100px', width:'100%', bottom:0}}>
       <Links/>
       <div style={{textAlign:'center', padding: "15px 0"}}>
-        <span>&copy; 2023 likarajo | <a href="/#/contact" style={{fontWeight:500, textDecoration:null}}>Contact</a></span>
+        <span>&copy; {year} likarajo | <a href="/#/contact" style={{fontWeight:500, textDecoration:null}}>Contact</a></span>
       </div>
     </div>
   )
